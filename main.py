225ab35e207f58703e0983dc32022d0f41503e14
Yzz09141 <a0980114963@gmail.com>
2025-04-25 23:12:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -219,6 +219,6 @@ def handle_message(event):
         return
 
     line_bot_api.reply_message(event.reply_token, TextSendMessage(
-        text="請輸入包含『莊』『閒』『和』的牌路，例如：莊閒莊莊閒
-或輸入：AI / 原始邏輯 切換模式"
-    ))
+    text="請輸入包含『莊』『閒』『和』的牌路，例如：莊閒莊莊閒\n或輸入：AI / 原始邏輯 切換模式"
+))
+
